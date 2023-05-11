b7c31fd49dbaadda11b6554dc8858d00cc0a01b0
taodx <taodx.hiblue@gmail.com>
2023-05-12 02:22:49 +0700

    .

--- js/music-list.js
+++ js/music-list.js
@@ -1,28 +1,14 @@
 let allMusic = [
-
-    {
-        name: "Bỏ em vào balo - Tajo cover",
-        artist: "Tajo",
-        img: "tao",
-        src: "boemvaobalo"
-    },
-    {
-        name: "Khi cô đơn em nhớ ai - Tajo cover",
-        artist: "Tajo",
-        img: "tao",
-        src: "kcd"
-    },
-    {
-        name: "Cô gái gucci - Tajo cover",
-        artist: "Tajo",
-        img: "tao",
-        src: "cogaiguci"
-    },
-    {
-        name: "Hạ còn vương nắng - Tajo cover",
-        artist: "Tajo",
-        img: "tao",
-        src: "hcvn"
-    },
-    ];
-    
+  {
+    name: "Cô gái gucci - Tajo cover",
+    artist: "Tajo",
+    img: "tao",
+    src: "cogaiguci",
+  },
+  {
+    name: "Hạ còn vương nắng - Tajo cover",
+    artist: "Tajo",
+    img: "tao",
+    src: "hcvn",
+  },
+];
